14f1d5434dda03a207e981b39208bb0949ffc7e1
dong <vuvandong10022004@gmail.com>
2026-01-06 14:59:42 +0700

    Bài tập buổi 22 update

--- BaiTapBuoi22/Lesson_3.js
+++ BaiTapBuoi22/Lesson_3.js
@@ -1,10 +1,12 @@
 // Lesson 3: Even/Odd Check
 // Write a single line of code using the ternary operator to check variable n. If n is even, return the string "Even", otherwise return "Odd".
 
-
-function checkEvenOdd(n) {
-		return n % 2 === 0 ? "Even" : "Odd"
+function isEvenNumber(n) {
+		return n % 2 === 0
 }
 
-// Example
-console.log(checkEvenOdd(9))
+// Example usage
+let n = 8
+
+let result = isEvenNumber(n) ? "Even" : "Odd"
+console.log(result)
